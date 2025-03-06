b869a1bd4890de7728b79f37b9e8cfaaaecbae21
Assia <na_farah@esi.dz>
2025-03-06 11:15:25 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import teacherRoutes from './routes/teacherRoutes.js';
+import studentRouters from  './routes/studentRoutes.js';
 import appConfig from './config/appConfig.js';
 
 //Imen : configuring the app 
@@ -7,7 +8,8 @@ const PORT = process.env.PORT || 7000 ;
 const app = express();
 appConfig(app);
 
+
 //Imen : Routes 
 app.use('/api/teachers',teacherRoutes);
-
+app.use('/api/students',studentRoutes));
 export default app ;
