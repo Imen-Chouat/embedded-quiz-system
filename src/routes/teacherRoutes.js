a8d7049e352c1741a68b0c9b1d7c3d358afc8098
Chouat Imen <164532073+Imen-Chouat@users.noreply.github.com>
2025-03-07 10:35:24 +0100

    Update teacherRoutes.js

--- src/routes/teacherRoutes.js
+++ src/routes/teacherRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import teacherControllers from '../controllers/teacherController.js';
+import { authTeacherMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register',teacherControllers.registerTeacher);
 router.post('/login',teacherControllers.loginTeacher);
-router.patch('/modifyName',teacherControllers.modifyName);// Imen : updating the name feild 
-
+router.patch('/modifyName',authTeacherMiddleware,teacherControllers.modifyName);// Imen : updating the name feild 
+router.patch('/modifySurName',authTeacherMiddleware,teacherControllers.modifySurName);
+router.patch('/modifyPassword',authTeacherMiddleware,teacherControllers.modifyPassword);
 export default router;
