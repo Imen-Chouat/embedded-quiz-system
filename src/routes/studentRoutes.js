bfed7f9487e0bd5c90a15cc25803561f6eac0dee
Chouat Imen <164532073+Imen-Chouat@users.noreply.github.com>
2025-03-07 10:23:17 +0100

    Update studentRoutes.js
    insuring the right importing of the student auth middleware

--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import studentControllers from '../controllers/studentController.js';
-import authenticateStudent from '../middlewares/authMiddleware.js';
+import { authenticateStudent } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 
