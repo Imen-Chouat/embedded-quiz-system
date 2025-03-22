ad997db99881ee7f6b593cd245c0437ad818e95e
zouak syrine <ns_zouak@esi.dz>
2025-03-23 00:06:49 +0100

    Update resultRouts.js

--- src/routes/resultRouts.js
+++ src/routes/resultRouts.js
@@ -3,7 +3,6 @@ import resultControllers from '../controllers/resultController.js';
 
 const router = express.Router();
 
-// 📌 Route pour obtenir le nombre d'étudiants ayant participé à un quiz
 router.get('/quizAttendance/:quiz_id', resultControllers.getQuizAttendance);
 
 export default router;
