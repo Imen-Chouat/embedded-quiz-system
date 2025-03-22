44a3d8c7c0cf1251a59218d1c989d1432f81f45e
zouak syrine <ns_zouak@esi.dz>
2025-03-23 00:28:49 +0100

    Update resultController.js

--- src/controllers/resultController.js
+++ src/controllers/resultController.js
@@ -3,14 +3,25 @@ import Result from '../modules/Result.js';
 export const getQuizAttendance = async (req, res) => {
     try {
         const { quiz_id } = req.params;
-        const attendance = await Result.getQuizAttendance(quiz_id); // Utilisation correcte
-        res.json(attendance);
+        const attendance = await Result.getQuizAttendance(quiz_id);
+        res.json({ quiz_id, attendance });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+export const getAverageQuizGrade = async (req, res) => {
+    try {
+        const { quiz_id } = req.params;
+        const averageGrade = await Result.getAverageQuizGrade(quiz_id);
+        res.json({ quiz_id, average_grade: averageGrade });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
 
 export default {
-    getQuizAttendance
+    getQuizAttendance,
+    getAverageQuizGrade
 };
   
