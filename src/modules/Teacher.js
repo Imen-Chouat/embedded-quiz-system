dbfce8d7404736eca6c1e0bacd8aaf5553f65a09
Chouat Imen <164532073+Imen-Chouat@users.noreply.github.com>
2025-03-23 10:35:04 +0100

    Update Teacher.js

--- src/modules/Teacher.js
+++ src/modules/Teacher.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2';
 import pool from '../config/dbConfig.js';
 //| id | name     |surname         | email               | password_hash | created_at  
-// IMEN : export async function create.. //other way 
+// IMEN : export async function create.. 
 class Teacher {
     static async create(name,surname,email,password_hash) {
         try {
@@ -37,6 +37,14 @@ class Teacher {
             throw new Error(`Error fetching teacher by ID: ${error.message}`);
         }
     }
+    static async updateRefreshToken(id,refreshToken){
+        try {
+            const [rows] = await pool.execute(`UPDATE teachers SET refresh_token = ? WHERE id = ?`,[refreshToken,id]);
+            return rows.affectedRows ;
+        } catch (error) {
+            throw new Error(`Error updating the refreshToken: ${error.message}`);
+        }
+    }
     static async searchByemail(email){
         try{
             const [result] = await pool.execute(`SELECT * FROM teachers WHERE email = ?`,[email]);
@@ -45,7 +53,7 @@ class Teacher {
             }
             return null ;
         }catch(error){
-            throw new Error(`Error fetching teacher by Email: ${error.message}`);
+            throw new Error(`Error fetching teacher by ID: ${error.message}`);
         }
     }
     static async getById(id){
@@ -59,6 +67,89 @@ class Teacher {
         } catch (error) {
             throw new Error(`Error fetching teacher by ID: ${error.message}`);
             return null;
+        }
+    }
+    static async getName(id){
+        try {
+            const [teacher] = await pool.execute(`SELECT name FROM teachers WHERE id = ?`,[id]);
+            if(teacher.length === 0){
+                return null ;
+            }else{
+                const teacherFound = teacher[0] ;
+                return teacherFound.name;
+            }
+        } catch (error) {
+            throw new Error(`Error fetching teacher name: ${error.message}`);
+        }
+    }
+    static async getSurName(id){
+        try {
+            const [teacher] = await pool.execute(`SELECT surname FROM teachers WHERE id = ? ;`,[id]);
+            if(teacher.length === 0){
+                return null ;
+            }else{
+                const teacherFound = teacher[0] ;
+                return teacherFound.surname;
+            }
+        } catch (error) {
+            throw new Error(`Error fetching teacher surname: ${error.message}`);
+        }
+    }
+    static async getRefreshToken(id){
+        try {
+            const [teacher] = await pool.execute(`SELECT refresh_token FROM teachers WHERE id = ? `,[id]);
+            if(teacher.length > 0){
+                const teacherFound = teacher[0] ;
+                return teacherFound.refresh_token;
+            }else {
+                return null ;
+            }
+        } catch (error) {
+            throw new Error(`Error fetching teacher refreshToken: ${error.message}`);
+        }
+    }
+    static async getCreatedAt(id){
+        try {
+            const [teacher] = await pool.execute(`SELECT created_at FROM teachers WHERE id = ?`,[id]);
+            if(teacher.length > 0){
+                const teacherFound = teacher[0];
+                return teacherFound.created_at ;
+            }else {
+                return null ;
+            }
+        } catch (error) {
+            throw new Error(`Error fetching teacher created at: ${error.message}`);            
+        }
+    }
+    static async getTeacherModules(id){
+        try {
+            const [result] = await pool.execute(`SELECT m.id, m.moduleName FROM teach_module tm JOIN modules m ON tm.module_id = m.id WHERE tm.teacher_id = ?`,[id]);
+            return result.length > 0 ? result : null ;
+        } catch (error) {
+            throw new Error(`Error fetching teacher module: ${error.message}`);            
+        }
+    }
+    static async addTeacherModule(teacher_id,module_id){
+        try {
+            const [rows] = await pool.execute(`INSERT INTO teach_module (teacher_id ,module_id) VALUES (?,?)`,[teacher_id,module_id]);
+            return rows.affectedRows ;
+        } catch (error) {
+            throw new Error(`Error adding a module to teacher: ${error.message}`);              
+        }
+    }
+    static async deleteTeacherModule(teacher_id,module_id){
+        try {
+            const [rows] = await pool.execute(`DELETE FROM teach_module WHERE teacher_id = ? AND module_id = ?`,[teacher_id,module_id]);
+            return rows.affectedRows ;
+        } catch (error) {
+            throw new Error(`Error deleting module from teacher: ${error.message}`);  
+        }
+    }
+    static async delete(id){
+        try {
+            const [rows] = await pool.execute(`DELETE FROM teachers WHERE id = ?`,[id]);
+        } catch (error) {
+            throw new Error(`Error creating teacher by ID: ${error.message}`);
         }
     }
 }
