7310abf3fa06355270333a8d96709fece22fef55
Assia <na_farah@esi.dz>
2025-03-24 05:04:46 +0100

    Update quizRoutes.js

--- src/routes/quizRoutes.js
+++ src/routes/quizRoutes.js
@@ -1,15 +1,25 @@
 import express from 'express';
 import quizController from '../controllers/quizController.js';
-import authMiddleware from '../middlewares/authMiddleware.js';
+import authenticateStudent, { authTeacherMiddleware } from '../middlewares/authMiddleware.js';
+
 
 const router = express.Router();
-router.post('/create', authMiddleware, quizController.createQuiz);
-router.delete('/:id', authMiddleware, quizController.deleteQuiz);
-router.patch('/:id/title', authMiddleware, quizController.update_title);
-router.patch('/:id/module', authMiddleware, quizController.update_module);
-router.patch('/:id/duration', authMiddleware, quizController.update_duration);
-router.patch('/:id/status', authMiddleware, quizController.update_status);
-router.patch('/:id/timedby', authMiddleware, quizController.update_timedby);
-router.get('/:quizId/randomize', authMiddleware, quizController.randomazation);
+
+
+
+
+router.delete('/:id', authTeacherMiddleware, quizController.deleteQuiz);
+router.patch('/:id/title', authTeacherMiddleware, quizController.update_title);
+router.patch('/:id/duration', authTeacherMiddleware, quizController.update_duration);
+router.patch('/:id/status', authTeacherMiddleware, quizController.update_status);
+router.patch('/:id/timedby', authTeacherMiddleware, quizController.update_timedby);
+router.get('/Allmodule', authTeacherMiddleware, quizController.ALLQuizzes);
+router.get('/draftmodule', authTeacherMiddleware, quizController.Draft_Quizzes);
+router.get('/Pastmodule', authTeacherMiddleware, quizController.Past_Quizzes);
+router.post("/start",authenticateStudent , quizController.startQuiz);
+router.post("/submit", authenticateStudent, quizController.submitQuizManually);
+router.post("/auto-submit", authenticateStudent, quizController.autoSubmitQuiz);
+router.post("/start-quiz", authTeacherMiddleware, quizController.startQuizTeach);
+
 
 export default router;
