da8b9a98ffb7c644304f4046062978877dcdd89e
Chouat Imen <164532073+Imen-Chouat@users.noreply.github.com>
2025-03-29 11:46:43 +0100

    Update resultController.js

--- src/controllers/resultController.js
+++ src/controllers/resultController.js
@@ -1,5 +1,5 @@
 import Result from '../modules/Result.js';
-
+import Answer from '../modules/Answer.js';
 export const getQuizAttendance = async (req, res) => {
     try {
         const { quiz_id } = req.params;
@@ -28,11 +28,25 @@ xport const getQuizSuccessRate = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
-
+export const questionChoicePercentage = async (req ,res) => {
+    try {
+        const {question_id} = req.body ;
+        let answers = await Answer.getAnswersByQuestionId(question_id);
+        let result = [];
+        for (let answer of answers) {
+            let percentage = await Result.choicePercentage(question_id, answer.id);
+            result.push({ answer_id: answer.id , answer_text: answer.answer_text, percentage });
+        }
+        res.status(200).json({ question_id, answersPercentages: result });
+    } catch (error) {
+        res.status(500).json({ message:'error finding the choices percentage'});
+    }
+}
 
 export default {
     getQuizAttendance,
     getAverageQuizGrade,
-    getQuizSuccessRate
+    getQuizSuccessRate,
+    questionChoicePercentage 
 };
   
