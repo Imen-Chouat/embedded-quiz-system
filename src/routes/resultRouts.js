e4f2ab4371cc9eb4ddf535b351e21a5231ee8c07
zouak syrine <ns_zouak@esi.dz>
2025-04-24 22:59:39 +0100

    Update resultRouts.js

--- src/routes/resultRouts.js
+++ src/routes/resultRouts.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get('/quizAttendance/:quiz_id', resultControllers.getQuizAttendance);
 router.get('/quizAverageGrade/:quiz_id', resultControllers.getAverageQuizGrade);
 router.get('/quizSuccessRate/:quiz_id', resultControllers.getQuizSuccessRate);
+router.get('/quiz/:quizId/student/:studentId/score', resultControllers.getScore);
 
 
 export default router;
