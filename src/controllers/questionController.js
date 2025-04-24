5ee8ea5b41c141d87f7a833c6dc8a44fca3d1784
Assia <na_farah@esi.dz>
2025-04-24 12:01:06 +0200

    Update questionController.js

--- src/controllers/questionController.js
+++ src/controllers/questionController.js
@@ -10,16 +10,12 @@ import Quiz from '../modules/Quiz.js';
         const newQuestion = await Question.createQuestion({ quiz_id, question_text, duration_seconds, grade });
      //plz add this one in order to handle the quiz duration 
     
-/*
+
        const quiz = await Quiz.findById(quiz_id);
         if (quiz && quiz.timed_by === 'question') {
             const totalDuration = await Quiz.calculateTotalDuration(quiz_id);
             await Quiz.updateQuizDuration(quiz_id, totalDuration);
         }
-
-    */
-
-     
         res.status(201).json(newQuestion);
     } catch (error) {
         res.status(500).json({ error: error.message });
