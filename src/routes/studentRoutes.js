5601c8923b6b630d9ee77df730886ccd0e1d874d
Assia <na_farah@esi.dz>
2025-04-24 02:39:45 +0200

    Update studentRoutes.js

--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -7,10 +7,11 @@ const router = express.Router();
 
 router.post('/register',studentControllers.registerStudent );
 router.post('/login',studentControllers.loginStudent );
+router.delete('/delete', authenticateStudent ,studentControllers.deleteAccount);
 router.patch('/modify_LastName',authenticateStudent ,studentControllers.modify_LastName );
 router.patch('/modify_FirstName',authenticateStudent ,studentControllers.modify_FirstName );
 router.patch('/modify_password',authenticateStudent ,studentControllers.modify_password );
-router.patch('/modify_group',authenticateStudent ,studentControllers.modify_group );
+router.patch('/modify_group',authenticateStudent ,studentControllers.updateStudentGroup  );
 router.get('/reviewQuiz',authenticateStudent ,studentControllers.reviewQuiz );
 
 export default router;
