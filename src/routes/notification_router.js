ccac8d340edd790135a0bf21b9a12cc5f06efb82
zouak syrine <ns_zouak@esi.dz>
2025-04-25 20:46:59 +0100

    Update notification_router.js

--- src/routes/notification_router.js
+++ src/routes/notification_router.js
@@ -3,7 +3,7 @@ import notificationController from '../controllers/notificationController.js';
 
 const router = express.Router();
 
-router.post('/notify-new-quiz', notificationController.notifyNewQuiz);
-router.get('/check-new-quiz', notificationController.checkNewQuiz);
+router.post('/notify-new-quiz/:quizId', notificationController.notifyNewQuiz);
 
+router.get('/check-new-quiz/:studentId', notificationController.checkNewQuiz);
 export default router;
