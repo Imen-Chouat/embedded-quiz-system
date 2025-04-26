88b07bd88dac7b7483f7f1893d422ca92690ca9b
Chouat Imen <164532073+Imen-Chouat@users.noreply.github.com>
2025-04-26 11:21:23 +0100

    Update teacherRoutes.js

--- src/routes/teacherRoutes.js
+++ src/routes/teacherRoutes.js
@@ -8,15 +8,19 @@ const upload = multer({dest : 'uploads/'});
 
 router.post('/register',teacherControllers.registerTeacher);
 router.post('/login',teacherControllers.loginTeacher);
-router.post('/refresh',authController.refreshAccessToken);
-router.post('/logout',authTeacherMiddleware,authController.logoutTeacher);
+router.post('/getFirstName',authTeacherMiddleware,teacherControllers.getFirstName);
+router.post('/getLastName',authTeacherMiddleware,teacherControllers.getLastName);
+router.post('/getEmail',authTeacherMiddleware,teacherControllers.getEmail);
 router.patch('/modifyName',authTeacherMiddleware,teacherControllers.modifyName);// Imen : updating the name feild 
 router.patch('/modifySurName',authTeacherMiddleware,teacherControllers.modifySurName);
 router.patch('/modifyPassword',authTeacherMiddleware,teacherControllers.modifyPassword);
-router.post('/addModule',teacherControllers.addTeacherModule);
-router.delete('/deleteModule',teacherControllers.deleteTeacherModule);
-router.patch('/updateModuleLevel',teacherControllers.updateModuleLevel);
-router.patch('/updateModuleName',teacherControllers.updateModuleName);
+router.post('/refresh',authController.refreshAccessToken);
+router.post('/logout',authTeacherMiddleware,authController.logoutTeacher);
+router.post('/addModule',authTeacherMiddleware,teacherControllers.addTeacherModule);
+router.delete('/deleteModule',authTeacherMiddleware,teacherControllers.deleteTeacherModule);
+router.patch('/updateModuleLevel',authTeacherMiddleware,teacherControllers.updateModuleLevel);
+router.patch('/updateModuleName',authTeacherMiddleware,teacherControllers.updateModuleName);
 router.post('/upload-students', upload.single('file'), teacherControllers.uploadStudentFile);
-router.delete('/deleteAccount',teacherControllers.deleteAccount);
+router.delete('/deleteAccount',authTeacherMiddleware,teacherControllers.deleteAccount);
+router.get('/getModules',authTeacherMiddleware,teacherControllers.getTeachermodules);
 export default router;
