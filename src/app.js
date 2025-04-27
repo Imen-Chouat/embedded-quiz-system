8afec023dd4460925b8e1b23256ea89b67bc196c
Chouat Imen <164532073+Imen-Chouat@users.noreply.github.com>
2025-04-27 18:57:27 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 import express from 'express';
 import teacherRoutes from './routes/teacherRoutes.js';
-import studentRouters from  './routes/studentRoutes.js';
+import appConfig from './config/appConfig.js';
 import question_router from './routes/question_router.js';
 import answer_router from './routes/answer_router.js';
-import appConfig from './config/appConfig.js';
+import studentRoutes from './routes/studentRoutes.js';
+import resultRoutes from './routes/resultRoutes.js';
+import quizRoutes from './routes/quizRoutes.js';
+import cookieParser from 'cookie-parser';
 
 //Imen : configuring the app 
 const PORT = process.env.PORT || 7000 ;
 const app = express();
 appConfig(app);
 
-
 //Imen : Routes 
+app.use(cookieParser());
 app.use('/api/teachers',teacherRoutes);
-app.use('/api/students',studentRoutes);
-app.use('/api/questions', question_router);
+app.use('/api/students',studentRoutes); 
+app.use('/api/result',resultRoutes);
+app.use('/api/quizzes',quizRoutes);
 app.use('/api/answers', answer_router); 
+app.use('/api/questions', question_router);
+
 export default app ;
