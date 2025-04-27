ea06ba5620b331a3b7f7a53ab394406cc3da8283
Assia <na_farah@esi.dz>
2025-04-27 07:19:31 +0200

    Update Student.js

--- src/modules/Student.js
+++ src/modules/Student.js
@@ -3,11 +3,11 @@ import pool from '../config/dbConfig.js';
 import bcryptjs from 'bcryptjs';
 
 class Student {
-    static async create(first_name, last_name, email, password_hash, group_id = null) {
+    static async create(first_name, last_name, email, password_hash) {
         try {
             const [row] = await pool.execute(
-                `INSERT INTO students (first_name, last_name, email, password_hash, group_id) VALUES (?, ?, ?, ?, ?)`,
-                [first_name, last_name, email, password_hash, group_id]
+                `INSERT INTO students (first_name, last_name, email, password_hash) VALUES (?, ?, ?, ?)`,
+                [first_name, last_name, email, password_hash]
             );
             console.log("Insert result:", row);
             return row.insertId;
@@ -20,7 +20,7 @@ class Student {
     static async update_Password(studentId , password) {
         try {
            const password_hash = await bcryptjs.hash(password, 8);
-            const [result] = await pool.execute(`UPDATE students SET password = ? WHERE id = ?`, [password_hash, studentId ]);
+            const [result] = await pool.execute(`UPDATE students SET password_hash = ? WHERE id = ?`, [password_hash, studentId ]);
             return result.affectedRows;
         } catch (error) {
             console.log(error);
@@ -47,7 +47,7 @@ class Student {
 
     static async update_groupid(id, group_id) {
         try {
-            const [result] = await pool.execute(`UPDATE students SET group_id = ? WHERE id = ?`, [group_id, id]);
+            const [result] = await pool.execute(`UPDATE student_group_csv SET group_id = ? WHERE id = ?`, [group_id, id]);
             return result.affectedRows;
         } catch (error) {
             console.log(error);
@@ -88,10 +88,76 @@ class Student {
             throw new Error("Failed to search student by email");
         }
     }
+// getters
+static async getfirst_name(id){
+    try {
+        const [student] = await pool.execute(`SELECT first_name FROM students WHERE id = ?`,[id]);
+        if(student.length === 0){
+            return null ;
+        }else{
+            const studentFound = student[0] ;
+            return studentFound.first_name;
+        }
+    } catch (error) {
+        throw new Error(`Error fetching student first_name: ${error.message}`);
+    }
+}
+static async getlast_name(id){
+    try {
+        const [student] = await pool.execute(`SELECT last_name FROM students WHERE id = ? ;`,[id]);
+        if(student.length === 0){
+            return null ;
+        }else{
+            const studentFound = student[0] ;
+            return studentFound.last_name;
+        }
+    } catch (error) {
+        throw new Error(`Error fetching student last_name : ${error.message}`);
+    }
 }
 
+static async getCreatedAt(id){
+    try {
+        const [studnet] = await pool.execute(`SELECT created_at FROM students WHERE id = ?`,[id]);
+        if(student.length > 0){
+            const studentFound = student[0];
+            return studentFound.created_at ;
+        }else {
+            return null ;
+        }
+    } catch (error) {
+        throw new Error(`Error fetching student created at: ${error.message}`);            
+    }
+}
+static async updateRefreshToken(id,refreshToken){
+    try {
+        const [rows] = await pool.execute(`UPDATE students SET refrech_token = ? WHERE id = ?`,[refreshToken,id]);
+        return rows.affectedRows ;
+    } catch (error) {
+        throw new Error(`Error updating the refreshToken: ${error.message}`);
+    }
+}
+static async getRefreshToken(id){
+    try {
+        const [student] = await pool.execute(`SELECT refrech_token FROM students WHERE id = ? `,[id]);
+        if(student.length > 0){
+            const studentFound = student[0] ;
+            return studentFound.refresh_token;
+        }else {
+            return null ;
+        }
+    } catch (error) {
+        throw new Error(`Error fetching teacher refreshToken: ${error.message}`);
+    }
+}
+static async delete(id){
+    try {
+        const [rows] = await pool.execute(`DELETE FROM students WHERE id = ?`,[id]);
+    } catch (error) {
+        throw new Error(`Error deleting student by ID: ${error.message}`);
+    }
+}
+
+}
 export default Student;
-
-
-
 
