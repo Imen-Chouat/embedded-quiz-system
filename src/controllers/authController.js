88adb6ed3e64e71cf059cc35ea32112f62d9cea6
Assia <na_farah@esi.dz>
2025-04-28 20:00:50 +0200

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,13 @@
-import jwt from 'jsonwebtoken';
 import envConfig from '../config/envConfig.js';
+import jwt from 'jsonwebtoken';
 import Teacher from '../modules/Teacher.js';
+import student  from '../modules/Student.js';
 
 const generateTokensTeacher= (teacher)=>{
-    const accessToken = jwt.sign({id:teacher.id , email:teacher.email},envConfig.JWT_SECRET,{expiresIn : '7d'});
+    const accessToken = jwt.sign({id:teacher.id , email:teacher.email},envConfig.JWT_SECRET,{expiresIn : '90h'});
     const refreshToken = jwt.sign({id:teacher.id,email:teacher.email},envConfig.REFRESH_TOKEN_SECRET,{expiresIn:'90d'});
     return {accessToken , refreshToken} ;
-} 
+}
 const refreshAccessToken = (req,res)=>{
     try {
         const {refreshToken} = req.cookies;
@@ -18,7 +19,7 @@ const refreshAccessToken = (req,res)=>{
                 return res.status(403).json({ message: "Invalid refresh token" });
             }
             const {id,email} = decoded;
-            const token = jwt.sign({id,email},envConfig.JWT_SECRET,{expiresIn:'15m'});
+            const token = jwt.sign({id,email},envConfig.JWT_SECRET,{expiresIn:'90h'});
             return res.status(200).json({"message":"access token refreshed successfully!",token});
         })
     } catch (error) {
@@ -38,9 +39,32 @@ const logoutTeacher = async (req, res) => {
     } catch (error) {
         return res.status(500).json({ message: "Error Logging out!" });
     }
-};
+}
+ const generateTokenStudent= (student)=>{
+        const accessToken = jwt.sign({id:student.id , email:student.email},envConfig.JWT_SECRET,{expiresIn : '90h'});
+        const refreshToken = jwt.sign({id:student.id,email:student.email},envConfig.REFRESH_TOKEN_SECRET,{expiresIn:'90d'});
+        return {accessToken , refreshToken} ;
+    }
+    
+    const logoutStudent = async (req, res) => {
+        try {
+            const {refreshToken} = req.cookies ;
+            if(!refreshToken){
+                return res.status(401).json({ message: "Unauthorized" });
+            }
+            const {id} = req.body;
+            await Student.updateRefreshToken(id,null);
+            res.clearCookie("refreshToken",{httpOnly: true, secure: true, sameSite: "Strict"});
+            return res.status(200).json({ message: "Logged out successfully!" });
+        } catch (error) {
+            return res.status(500).json({ message: "Error Logging out!" });
+        }
+    }
 export default {
     logoutTeacher,
     refreshAccessToken,
-    generateTokensTeacher
+    generateTokensTeacher , 
+    logoutStudent,
+    generateTokenStudent 
+
 }
